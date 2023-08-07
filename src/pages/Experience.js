916fa3f72198daab37101550ecc00a9d446add11
aro126 <ambrenott@outlook.com>
2023-08-07 02:18:56 -0400

    Updated experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -12,17 +12,69 @@ function Experience() {
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2010-2014"
+          date="2023"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Random High School, Random Place, Random State
+            B.S. in Computer Science
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            Subtitle
+            Georgia Institute of Technology
           </h4>
-          <p>High School Diploma</p>
+          <p>Obtained degree in Computer Science. Coursework 
+            included Object Oriented Design, Data Structures and 
+            Algorithms, Discrete Mathematics, etc.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2022-2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            UX Design Certificate
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Google Career Certificate
+          </h4>
+          <p>Currently completing Google's UX certificate to gain
+            more knowledge of design principles (largely for personal projects).
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019-2020"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Teaching Assistant
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Georgia Institute of Technology
+          </h4>
+          <p>Assisted with the Intro to Object Oriented Programming 
+            course. This course is taught in Python and introduces 
+            students to common data types and structures.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2020"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Software Development Engineer Intern
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Amazon
+          </h4>
+          <p>Worked on Amazon’s Item Master Service team dealing 
+            with catalog items. During this internship, I gained 
+            experience working with databases as well as Amazon’s 
+            item processing system.</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
