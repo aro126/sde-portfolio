5cf2e6e28aabe64470a1de5776c6cc3e5d2c8792
aro126 <ambrenott@outlook.com>
2023-08-07 00:56:50 -0400

    Project page formatted

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,34 @@
+import SampleImg from "../assets/warm_colorful.jpeg";
+
+export const ProjectList = [
+    {
+        name: "Project1",
+        image: SampleImg,
+        skills: "example, example"
+    },
+    {
+        name: "Project2",
+        image: SampleImg,
+        skills: "example, example"
+    },
+    {
+        name: "Project3",
+        image: SampleImg,
+        skills: "example, example"
+    },
+    {
+        name: "Project4",
+        image: SampleImg,
+        skills: "example, example"
+    },
+    {
+        name: "Project5",
+        image: SampleImg,
+        skills: "example, example"
+    },
+    {
+        name: "Project6",
+        image: SampleImg,
+        skills: "example, example"
+    }
+];
